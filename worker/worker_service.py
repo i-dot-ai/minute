8f6d3ad44e74ae2bed96fac020036671662f860e
Aydin Utting <aydin.utting@cabinetoffice.gov.uk>
2025-10-29 11:08:32 +0000

    fix: manually restart failed ray tasks
    * Split RayQueueReceiveService into Transcription and LLM service so they can both have the same interface (a `process` method) and therefore if a task fails we can just get the actor by its index in the actor array and run `process()` to restart it
    * Suppress errors when completing, deadlettering or abandoning messages due to expired receipt handles

--- worker/worker_service.py
+++ worker/worker_service.py
@@ -9,7 +9,7 @@ from common.services.queue_services import get_queue_service
 from common.services.queue_services.base import QueueService
 from common.settings import get_settings
 from common.types import TaskType
-from worker.ray_recieve_service import HasBeenStopped, RayQueueReceiveService
+from worker.ray_recieve_service import HasBeenStopped, RayLlmService, RayTranscriptionService
 from worker.signal_handler import SignalHandler
 
 logger = logging.getLogger(__name__)
@@ -19,24 +19,29 @@ settings = get_settings()
 class WorkerService:
     def __init__(self, queue_service: QueueService):
         self.queue_service = queue_service
-        self.remote_calls = []
+        self.actors = []
+        self.calls = []
         self.signal_handler = SignalHandler()
         self.llm_queue = Queue()
         self.transcription_queue = Queue()
         self.stopped = HasBeenStopped.remote()
         for _ in range(settings.MAX_TRANSCRIPTION_PROCESSES):
-            transcription_worker = RayQueueReceiveService.remote(self.queue_service, self.stopped)
-            transcription_worker_call = transcription_worker.process_transcription_task.remote(self.transcription_queue)
-            self.remote_calls.append(transcription_worker_call)
+            transcription_worker = RayTranscriptionService.remote(
+                self.queue_service, self.stopped, self.transcription_queue
+            )
+            transcription_worker_call = transcription_worker.process.remote()
+            self.actors.append(transcription_worker)
+            self.calls.append(transcription_worker_call)
 
         for _ in range(settings.MAX_LLM_PROCESSES):
-            llm_worker = RayQueueReceiveService.remote(self.queue_service, self.stopped)
-            llm_worker_call = llm_worker.process_llm_task.remote(self.llm_queue)
-            self.remote_calls.append(llm_worker_call)
+            llm_worker = RayLlmService.remote(self.queue_service, self.stopped, self.llm_queue)
+            llm_worker_call = llm_worker.process.remote()
+            self.actors.append(llm_worker)
+            self.calls.append(llm_worker_call)
 
     async def run(self) -> None:
         # note, currently a bug in python 3.12/ray that means we need to wrap the ObjectRefs in asyncio.ensure_future
-        all_calls_futures = [asyncio.ensure_future(call) for call in self.remote_calls]
+        futures = [asyncio.ensure_future(call) for call in self.calls]
         while not self.signal_handler.signal_received:
             messages = self.queue_service.receive_message()
             for message, receipt_handle in messages:
@@ -48,10 +53,18 @@ class WorkerService:
                     case _:
                         logger.warning("Message not recognised: %s , Sending to dead letter", message)
                         self.queue_service.deadletter_message(message, receipt_handle)
-            failed, pending = await asyncio.wait(all_calls_futures, timeout=1)
+            failed, pending = await asyncio.wait(futures, timeout=1)
             for task in failed:
-                # exc_info=False as this comes from another process
-                logger.error("Task has finished unexpectedly: error %s", task, exc_info=False)
+                # Manually restart failed jobs
+                try:
+                    # exc_info=False as this comes from another process
+                    logger.error("Task has finished unexpectedly: error %s", task, exc_info=False)
+                    idx = futures.index(task)
+                    self.calls[idx] = self.actors[idx].process.remote()
+                    futures[idx] = asyncio.ensure_future(self.calls[idx])
+
+                except Exception as e:  # noqa: BLE001
+                    logger.error("Failed to restart worker %s", e)
 
         logger.info("Signal recieved. Setting stopped to True")
         await self.stopped.set.remote()
@@ -65,7 +78,7 @@ class WorkerService:
             self.queue_service.abandon_message(receipt_handle)
 
         while True:
-            done, pending = await asyncio.wait(all_calls_futures, timeout=1)
+            done, pending = await asyncio.wait(futures, timeout=1)
             if not pending:
                 logger.info("No remaining jobs. Stopping.")
                 break

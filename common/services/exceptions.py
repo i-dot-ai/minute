970857288a2f2f5e449603e876905de01ed3ac61
Ryan White <ryan.white@ai.gov.uk>
2025-12-18 11:44:35 +0000

    fix exceptions file formatting

--- common/services/exceptions.py
+++ common/services/exceptions.py
@@ -5,5 +5,7 @@ class TranscriptionFailedError(Exception):
 class InteractionFailedError(Exception):
     """Exception raised when a transcription fails."""
 
+
 class MissingAuthTokenError(Exception):
+    """Exception raised when an auth token is not provided where required."""
     pass

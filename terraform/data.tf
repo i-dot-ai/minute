7605d31954636eab414b9ad807e9a535534b6753
Danny Burke <danny.burke@dsit.gov.uk>
2026-04-09 08:34:08 +0100

    Enable Edge Networking and remove IP whitelists

--- terraform/data.tf
+++ terraform/data.tf
@@ -55,6 +55,7 @@ data "terraform_remote_state" "keycloak" {
 locals {
   name              = "${var.team_name}-${var.env}-${var.project_name}"
   host              = terraform.workspace == "prod" ? "${var.project_name}.ai.cabinetoffice.gov.uk" : "${var.project_name}-${terraform.workspace}.ai.cabinetoffice.gov.uk"
+  public_host       = terraform.workspace == "prod" ? "${var.project_name}.i.ai.gov.uk" : "${var.project_name}.${terraform.workspace}.i.ai.gov.uk"
   host_backend      = terraform.workspace == "prod" ? "${var.project_name}-backend-external.ai.cabinetoffice.gov.uk" : "${var.project_name}-backend-external-${terraform.workspace}.ai.cabinetoffice.gov.uk"
   record_prefix     = terraform.workspace == "prod" ? var.project_name : "${var.project_name}-${terraform.workspace}"
   auth_from_address = "${local.record_prefix}@auth-notify.${var.domain_name}"
@@ -79,4 +80,8 @@ data "aws_secretsmanager_secret" "slack" {
 
 data "aws_secretsmanager_secret_version" "platform_slack_webhook" {
   secret_id = data.aws_secretsmanager_secret.slack.id
+}
+
+data "aws_ssm_parameter" "edge_secret" {
+  name = "/i-dot-ai-${terraform.workspace}-core-edge-network/header-secret"
 }

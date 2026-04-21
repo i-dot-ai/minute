f2c2c296ed293675199359725c7fa579000481cc
Ryan White <ryan.white@ai.gov.uk>
2026-04-21 11:18:07 +0100

    add public host to listener

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -103,6 +103,7 @@ module "frontend" {
   vpc_id                       = data.terraform_remote_state.vpc.outputs.vpc_id
   private_subnets              = data.terraform_remote_state.vpc.outputs.private_subnets
   host                         = local.host
+  public_host                  = var.edge_networking_enabled ? local.public_host : null
   load_balancer_security_group = module.load_balancer.load_balancer_security_group_id
   aws_lb_arn                   = module.load_balancer.alb_arn
   ecs_cluster_id               = data.terraform_remote_state.platform.outputs.ecs_cluster_id

fdd683c015f8ef7357deedc0efb2f37a7f6719b5
Aydin Utting <aydin.utting@cabinetoffice.gov.uk>
2026-06-22 10:25:46 +0100

    Store recordings in restricted S3 path

--- backend/utils/get_file_s3_key.py
+++ backend/utils/get_file_s3_key.py
@@ -1,14 +1,12 @@
-def get_file_s3_key(user_email: str, file_name: str, file_type: str = "multimedia") -> str:
+def get_file_s3_key(user_email: str, file_name: str) -> str:
     """
     Generate a consistent S3 file key for user uploads.
 
     Args:
         user_email (str): The email of the user uploading the file
-        extension (str): The file extension
+        file_name (str): the name of the file to be uploaded
 
     Returns:
-        str: The generated file key in the format 'user-uploads/{email}/{uuid}.{extension}'
+        str: The generated file key in the format 'user-uploads/{email}/{file_name}'
     """
-    if file_type == "document":
-        return f"app_data/document-uploads/{user_email}/{file_name}"
-    return f"app_data/user-uploads/{user_email}/{file_name}"
+    return f"restricted/user-uploads/{user_email}/{file_name}"

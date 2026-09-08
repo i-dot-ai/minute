92003877c9ec3ec1b54dbeedc5e658ccf3e57c7c
jvickers123 <jonathanbvickers@gmail.com>
2026-09-08 13:13:44 +0100

    format file

--- common/database/postgres_models.py
+++ common/database/postgres_models.py
@@ -23,9 +23,7 @@ def created_datetime_column():
 
 
 def updated_datetime_column():
-    return Column(
-        TIMESTAMP(timezone=True), nullable=False, server_default=now(), default=None, onupdate=now()
-    )
+    return Column(TIMESTAMP(timezone=True), nullable=False, server_default=now(), default=None, onupdate=now())
 
 
 class BaseTableMixin(SQLModel):

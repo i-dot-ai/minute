83e4d6f1020a5116f55a4fc71aeaa6a79b18fd90
diego-peteiro-i-dot-ai <diego.peteiro@dsit.gov.uk>
2026-09-24 11:44:07 +0100

    Testing structured logging to OS

--- common/services/transcription_services/azure.py
+++ common/services/transcription_services/azure.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import time
 from pathlib import Path
 from typing import Any
@@ -6,6 +7,9 @@ from typing import Any
 import aiofiles
 import httpx
 import sentry_sdk
+from i_dot_ai_utilities.logging.structured_logger import StructuredLogger
+from i_dot_ai_utilities.logging.types.enrichment_types import ExecutionEnvironmentType
+from i_dot_ai_utilities.logging.types.log_output_format import LogOutputFormat
 from sentry_sdk.consts import SPANSTATUS
 from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_exponential
 
@@ -20,6 +24,22 @@ settings = get_settings()
 logger = logging.getLogger(__name__)
 url = f"https://{settings.AZURE_SPEECH_REGION}.api.cognitive.microsoft.com/speechtotext/transcriptions:transcribe"
 headers = {"Ocp-Apim-Subscription-Key": settings.AZURE_SPEECH_KEY}
+
+environment = os.environ.get("ENVIRONMENT")
+logger_env = ExecutionEnvironmentType.LOCAL if environment == "LOCAL" else ExecutionEnvironmentType.FARGATE
+logger_fmt = LogOutputFormat.TEXT if environment == "LOCAL" else LogOutputFormat.JSON
+
+logger = StructuredLogger(
+    level=logging.INFO,
+    options={
+        "execution_environment": logger_env,
+        "log_format": logger_fmt,
+    }
+)
+
+slogger = StructuredLogger()
+slogger.refresh_context()
+logger.info("A thing happened", thing_id=12345, user_logged_in=True, azure=True)
 
 
 class AzureSpeechAdapter(TranscriptionAdapter):
@@ -45,6 +65,8 @@ class AzureSpeechAdapter(TranscriptionAdapter):
         """
         Async version of transcribe audio using Azure Speech-to-Text API
         """
+
+        slogger.refresh_context()
 
         with sentry_sdk.start_transaction(op="process", name="read_file_before_azure_transcribe") as transaction:
             async with aiofiles.open(audio_file_path_or_recording, "rb") as audio_file:
@@ -81,6 +103,7 @@ class AzureSpeechAdapter(TranscriptionAdapter):
                 response = await client.post(url, headers=headers, files=files, params=params)
                 duration_ms = (time.monotonic() - start_time) * 1000
                 transaction.set_tag("azure_stt.status_code", response.status_code)
+                slogger.info("[TAG] azure_stt.status_code", azure_stt=1, status_code=response.status_code)
 
                 sentry_sdk.metrics.count(
                     "azure_stt.requests",
